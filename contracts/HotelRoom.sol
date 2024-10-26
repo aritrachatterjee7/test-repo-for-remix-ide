2dc77d7ffe32f643a5bd0337b743c43668af670d
aritrachatterjee7 <aritra.chatterjee.ece27@heritageit.edu.in>
2024-10-26 11:51:35 +0530

    new commit

---
+++ contracts/HotelRoom.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HotelRoom{
+
+    address payable  public owner;
+
+    constructor(){
+        
+    }
+
+    function book()public{
+        
+    }
+}
